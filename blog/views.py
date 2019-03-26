6c656976ca872ff0b9caff116ce9c2dcda45f926
karaname <rekhov.ka@gmail.com>
2019-03-26 13:53:33 +0300

    method show > get(pk=pk)/show.html

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from blog.models import Post
 def index(request):
 	posts = Post.objects.order_by('-created_at')
 	return render(request, 'blog/index.html', {'posts':posts})
+
+def show(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'blog/show.html', {'post':post})
