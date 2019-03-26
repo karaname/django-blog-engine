29e1f698f1137825dc47a28300d0d933f8c134b5
karaname <rekhov.ka@gmail.com>
2019-03-26 13:26:01 +0300

    create model/display order_by > posts(created_at) in view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Post(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+'''
+CharField() - для хранения коротких строк, принимает один дополнительный аргумент
+максимальную длинну строки
+TextField() - для большого кол-ва текстовой информации
+DateTimeField(auto_now_add=False/True) - принимает дату и время, есть ещё DateField
+только с датой
+'''
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from blog.models import Post
 
 def index(request):
-	return HttpResponse('Hello')
+	posts = Post.objects.order_by('-created_at')
+	return render(request, 'blog/index.html', {'posts':posts})
