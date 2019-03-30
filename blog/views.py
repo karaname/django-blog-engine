a258508d18f3eddad784544de0fa5bfb70d1dd74
karaname <rekhov.ka@gmail.com>
2019-03-30 20:23:18 +0300

    @login_required main function for authorization system

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 from blog.forms import PostForm
 from blog.models import Post
 
+from django.contrib.auth.decorators import login_required
+
+
 def index(request):
 	posts = Post.objects.order_by('-created_at')
 	return render(request, 'blog/index.html', {'posts':posts})
@@ -12,6 +15,7 @@ def show(request, pk):
 	post = Post.objects.get(pk=pk)
 	return render(request, 'blog/show.html', {'post':post})
 
+@login_required
 def new(request):
 	if request.method == 'GET':
 		form = PostForm()
@@ -24,6 +28,7 @@ def new(request):
 			new_post.save()
 			return redirect('blog:index')
 
+@login_required
 def update(request, pk):
 	post = Post.objects.get(id=pk)
 
@@ -34,10 +39,12 @@ def update(request, pk):
 
 	return render(request, 'blog/update.html', {'form':form})
 
+@login_required
 def delete(request, pk):
 	post = Post.objects.get(id=pk).delete()
 	return redirect('blog:index')
 
+@login_required
 def logout_view(request):
 	logout(request)
 	return redirect('blog:index')
