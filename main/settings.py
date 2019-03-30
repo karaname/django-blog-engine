753695e5075ee7d64216d5d62b18f738ca8b2fb0
karaname <rekhov.ka@gmail.com>
2019-03-30 16:19:27 +0300

    python manage.py show_urls

--- main/settings.py
+++ main/settings.py
@@ -12,12 +12,12 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions', # show_urls
     'blog',
 ]
 
