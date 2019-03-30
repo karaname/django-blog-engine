c09260b1e4629068cd1aba5bb77e310a28f513cb
karaname <rekhov.ka@gmail.com>
2019-03-30 20:46:54 +0300

    TemplateView/class structure > MainView | AuthView/template structure

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,17 @@
 from django.contrib.auth import views as auth_views
+from blog.views import MainView, AuthView
 from django.conf.urls import url
-from blog import views
 
 app_name = 'blog'
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^post/(?P<pk>[0-9A-Fa-f-]+)$', views.show, name='show'),
-	url(r'^post/new$', views.new, name='new'),
-	url(r'^post/update/(?P<pk>[0-9A-Fa-f-]+)$', views.update, name='update'),
-	url(r'^post/del/(?P<pk>[0-9A-Fa-f-]+)$', views.delete, name='delete'),
+	# MainView
+	url(r'^$', MainView.index, name='index'),
+	url(r'^post/(?P<pk>[0-9A-Fa-f-]+)$', MainView.show, name='show'),
+	url(r'^post/new$', MainView.new, name='new'),
+	url(r'^post/update/(?P<pk>[0-9A-Fa-f-]+)$', MainView.update, name='update'),
+	url(r'^post/del/(?P<pk>[0-9A-Fa-f-]+)$', MainView.delete, name='delete'),
 
-	url(r'^login/$', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-	url(r'^logout/$', views.logout_view, name='logout'),
+	# AuthView
+	url(r'^login/$', auth_views.LoginView.as_view(template_name='blog/auth/login.html'), name='login'),
+	url(r'^logout/$', AuthView.logout_view, name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,50 +1,51 @@
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.http import HttpResponse
 from blog.forms import PostForm
 from blog.models import Post
 
-from django.contrib.auth.decorators import login_required
+class MainView(TemplateView):
+	def index(request):
+		posts = Post.objects.order_by('-created_at')
+		return render(request, 'blog/main/index.html', {'posts':posts})
 
+	def show(request, pk):
+		post = Post.objects.get(pk=pk)
+		return render(request, 'blog/main/show.html', {'post':post})
 
-def index(request):
-	posts = Post.objects.order_by('-created_at')
-	return render(request, 'blog/index.html', {'posts':posts})
+	@login_required
+	def new(request):
+		if request.method == 'GET':
+			form = PostForm()
+			return render(request, 'blog/main/new.html', {'form': form})
+		else:
+			form = PostForm(request.POST)
 
-def show(request, pk):
-	post = Post.objects.get(pk=pk)
-	return render(request, 'blog/show.html', {'post':post})
+			if form.is_valid():
+				new_post = form.save(commit=False)
+				new_post.save()
+				return redirect('blog:index')
 
-@login_required
-def new(request):
-	if request.method == 'GET':
-		form = PostForm()
-		return render(request, 'blog/new.html', {'form': form})
-	else:
-		form = PostForm(request.POST)
+	@login_required
+	def update(request, pk):
+		post = Post.objects.get(id=pk)
 
+		form = PostForm(request.POST or None, instance=post)
 		if form.is_valid():
-			new_post = form.save(commit=False)
-			new_post.save()
+			form.save()
 			return redirect('blog:index')
 
-@login_required
-def update(request, pk):
-	post = Post.objects.get(id=pk)
+		return render(request, 'blog/main/update.html', {'form':form})
 
-	form = PostForm(request.POST or None, instance=post)
-	if form.is_valid():
-		form.save()
+	@login_required
+	def delete(request, pk):
+		post = Post.objects.get(id=pk).delete()
 		return redirect('blog:index')
 
-	return render(request, 'blog/update.html', {'form':form})
-
-@login_required
-def delete(request, pk):
-	post = Post.objects.get(id=pk).delete()
-	return redirect('blog:index')
-
-@login_required
-def logout_view(request):
-	logout(request)
-	return redirect('blog:index')
+class AuthView(TemplateView):
+	@login_required
+	def logout_view(request):
+		logout(request)
+		return redirect('blog:index')
