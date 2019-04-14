aa9f9d12ecedf31a3e8329f65cb41dabfafc25ed
karaname <rekhov.ka@gmail.com>
2019-04-14 22:21:20 +0300

    rewrited/renamed/structure

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -14,62 +14,48 @@ class PostListViewTest(TestCase):
 		cls.posts = Post.objects.all()
 		cls.user = User.objects.create_user('testuser', '', 'foobario')
 
-	def test_creation_check(self):
+	def test_creation_obj(self):
 		self.assertEqual(len(self.posts), 8)
 
-	def test_view_url_exists_at_desired_location(self):
-		resp = self.client.get('/')
-		self.assertEqual(resp.status_code, 200)
-
-	def test_view_url_accessible_by_name(self):
+	def test_url_index(self):
 		resp = self.client.get(reverse('blog:index'))
 		self.assertEqual(resp.status_code, 200)
-
-	def test_view_uses_correct_template(self):
-		resp = self.client.get(reverse('blog:index'))
-		self.assertEqual(resp.status_code, 200)
-
 		self.assertTemplateUsed(resp, 'blog/main/index.html')
 
-	def test_pagination_is_eight_first_p(self):
-		resp = self.client.get(reverse('blog:index'))
+	def test_pagination_first_page(self):
+		resp = self.client.get(reverse('blog:index')+'?page=1')
 		self.assertEqual(resp.status_code, 200)
 		self.assertTrue('posts' in resp.context)
-		self.assertEqual(len(resp.context['posts']), 4)
 		self.assertTrue(resp.context['posts'], 4)
+		self.assertEqual(len(resp.context['posts']), 4)
 
-	def test_pagination_is_eight_second_p(self):
+	def test_pagination_second_page(self):
 		resp = self.client.get(reverse('blog:index')+'?page=2')
 		self.assertEqual(resp.status_code, 200)
 		self.assertTrue('posts' in resp.context)
-		self.assertEqual(len(resp.context['posts']), 4)
 		self.assertTrue(resp.context['posts'], 4)
+		self.assertEqual(len(resp.context['posts']), 4)
 
-	def test_auth_url_new(self):
-		self.client.login(username='testuser', password='foobario')
-
-		resp = self.client.get('/post/new')
-		self.assertEqual(resp.status_code, 200)
-
-		self.assertEqual(str(resp.context['user']), 'testuser')
-
-		self.assertTemplateUsed(resp, 'blog/main/new.html')
-
-	def test_redirect_if_not_logged(self):
-		resp = self.client.get('/post/new')
-		self.assertEqual(resp.status_code, 302)
-		self.assertRedirects(resp, '/accounts/login/?next=/post/new', status_code=302, target_status_code=404)
-
-	def test_uuid(self):
-		for post in self.posts:
-			post_id = str(post.id)
-			self.assertEqual(len(post_id), 36)
-
-	def test_detail_url(self):
-		self.client.login(username='testuser', password='foobario')
-
+	def test_url_show(self):
 		resp = self.client.get(reverse('blog:show', kwargs={'pk':self.post.id}))
 		self.assertEqual(resp.status_code, 200)
 		self.assertTrue('post' in resp.context)
+		self.assertTemplateUsed(resp, 'blog/main/show.html')
 
+	def test_logged_user_url_new(self):
+		self.client.login(username='testuser', password='foobario')
+		resp = self.client.get(reverse('blog:new'))
+		self.assertEqual(resp.status_code, 200)
 		self.assertEqual(str(resp.context['user']), 'testuser')
+		self.assertTrue('form' in resp.context)
+		self.assertTemplateUsed(resp, 'blog/main/new.html')
+
+	def test_redirect_if_user_not_auth(self):
+		resp = self.client.get(reverse('blog:new'))
+		self.assertEqual(resp.status_code, 302)
+		self.assertRedirects(resp, '/accounts/login/?next=/post/new', status_code=302, target_status_code=404)
+
+	def test_each_obj_uuid(self):
+		for post in self.posts:
+			post_id = str(post.id)
+			self.assertEqual(len(post_id), 36)
